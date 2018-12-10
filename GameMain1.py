463593ca677ff79ab000f3b4c7a424a76c233301
Sushruth <sushrut.bhandary@gmail.com>
2018-12-09 23:42:14 -0800

    ships/ aquatic mines/ disembarking

--- GameMain1.py
+++ GameMain1.py
@@ -91,8 +91,95 @@ class GamePlatform:
         self.playerPlatoons = []
         self.enemyPlatoons = []
         self.activeCannonBalls=[]
+        self.ship_paths = []
+        self.island_nodes = []
+        self.water_nodes = []
+        self.disembarking_points = []
 
+# ------------------- FUCNTION TO FIND A ROUTE ----------------
 
+def find_route(pos, dest, node_list):
+    """ Given the initial position of a land unit, find a path from its position to an end position """
+
+    # Variable to hold the result path
+    result_path = []
+
+    # Get start position
+    x1 = pos[0]
+    y1 = pos[1]
+
+    # Initialize variables for loop
+    closest_node = None
+    distance = 2000  # Arbitrary large distance that can't exist between values for this display size
+
+    # Find closest start node
+    for node in node_list:
+        distance_from_start = calcDistance(x1, y1, node.point.x, node.point.y)
+        if (distance_from_start < distance):
+            distance = distance_from_start
+            closest_node = node
+    result_path.append(closest_node)
+    # print("start node #: ", closest_node.number)
+    source = closest_node.number
+
+    # Count nodes going forward (clockwise)
+    forward_list = []
+    # print("forward loop")
+    # Determine starting index
+    start = source
+    if source == len(node_list) - 1:
+        start = 0
+    # print("starting at node: ", start)
+    # Look for nodes
+    for i in range(start, len(node_list)):
+        if node_list[i].number == dest:
+            forward_list.append(node_list[i])
+            # print("appended node ", node_list[i].number)
+            break
+        forward_list.append(node_list[i])
+        # print("appended node ", node_list[i].number)
+        if i + 1 == len(node_list):
+            for j in range(0, start):
+                if node_list[j].number == dest:
+                    forward_list.append(node_list[i])
+                    # print("appended node ", node_list[j].number)
+                    break
+                forward_list.append(node_list[j])
+                # print("appended node ", node_list[j].number)
+
+    # Count nodes going backward (counter clockwise)
+    backward_list = []
+    # print("backward loop")
+    # Determine the starting index
+    if source == 0:
+        backward_list.append(node_list[source])
+        source = len(node_list) - 1
+    for i in range(source, 0, -1):
+        if node_list[i].number == dest:
+            backward_list.append(node_list[i])
+            # print("appended node ", node_list[i].number)
+            break
+        backward_list.append(node_list[i])
+        # print("appended node ", node_list[i].number)
+        if i == 0:
+            for j in range(len(node_list), source, -1):
+                if node_list[j].number == source or node_list[j].number == dest:
+                    backward_list.append(node_list[i])
+                    # print("appended node ", node_list[j].number)
+                    break
+                backward_list.append(node_list[j])
+                # print("appended node ", node_list[j].number)
+
+    # Compare which path is shorter
+    # print("length of forward: ", len(forward_list))
+    # print("length of backward: ", len(backward_list))
+
+    if len(forward_list) < len(backward_list):
+        return forward_list
+    else:
+        return backward_list
+
+# ------------------- /FUCNTION TO FIND A ROUTE ----------------
 
 if __name__ == '__main__':
 
@@ -163,12 +250,106 @@ if __name__ == '__main__':
 
     platform.enemyPlatoons.append(Platoon(platform,2,'enemy'))
 
-    #create 3 ships
-    platform.ship_list.append(Ship.Ship(100, 100, 0, 10))
-    platform.ship_list.append(Ship.Ship(1000, 100, 0, 10))
-    platform.ship_list.append(Ship.Ship(1200, 700, 0, 10))
-    for ship in platform.ship_list:
-        platform.all_sprites_list.add(ship)
+
+
+    disembark0 = [(350,450),(350,435),(350,415),(365,450),(365,435),(365,415)]
+    disembark1 = [(850, 660),(865, 660),(880, 660),(850, 645),(865, 645),(880, 645)]
+    disembark2 = [(1020, 305),(1005, 305),(990, 305),(1020, 330),(1005, 330),(990, 330)]
+    disembarks = [disembark0,disembark1,disembark2]
+    platform.disembarking_points = disembarks
+
+
+    # --- Determine routes for ships that will be used randomly for ships
+    g1 = Graph.Graph()
+    g2 = Graph.Graph()
+
+    # Adjacency list in the form of a matrix
+    graph1 = [[0, 5, 0, 0, 0, 10, 0, 0],
+              [0, 0, 5, 0, 0, 0, 0, 0],
+              [0, 0, 0, 5, 0, 0, 0, 0],
+              [0, 0, 0, 0, 5, 0, 0, 0],
+              [0, 0, 0, 5, 0, 0, 0, 30],
+              [0, 0, 0, 0, 0, 0, 5, 0],
+              [0, 0, 0, 0, 0, 0, 0, 10],
+              [0, 0, 0, 0, 0, 0, 0, 0]]
+    graph2 = [[0, 5, 0],
+              [0, 0, 5],
+              [0, 0, 0]]
+
+    # Node mapping and creates the shortest path from source to destination via graph 1
+    # node0 = Path.Node(0, Path.Point(100, 55))
+    # node1 = Path.Node(1, Path.Point(174, 287))
+    # node2 = Path.Node(2, Path.Point(160, 720))
+    # node3 = Path.Node(3, Path.Point(556, 779))
+    # node4 = Path.Node(4, Path.Point(1007, 672))
+    # node5 = Path.Node(5, Path.Point(678, 73))
+    # node6 = Path.Node(6, Path.Point(1092, 204))
+    # node7 = Path.Node(7, Path.Point(1224, 447))
+    # nodearray1 = [node0, node1, node2, node3, node4, node5, node6, node7]
+    # platform.water_nodes = nodearray1
+
+    node0=(100,100)
+    node1=(124,420)
+    node2=(265,758)
+    node3=(602,809)
+    node4=(898,758)
+    node5=(1090,251)
+    node6=(700,25)
+    node_array=[node0,node1,node2,node3,node4,node5,node6]
+    platform.water_nodes = node_array
+
+
+
+    # path_ship_1 = g1.dijkstra(graph1, 0, 1)
+    path_ship_1 = [1]
+    # path_ship_2 = g1.dijkstra(graph1, 0, 4)
+    path_ship_2 = [6,5]
+    # path_ship_3 = [0, 5, 6]
+    path_ship_3 = [1,2,3,4]
+
+    #debugging :
+    # print("here")
+    print(path_ship_1, path_ship_2, path_ship_3)
+
+    # Node mapping and creates the shortest path from source to destination via graph 1
+    node_one = Path.Node(0, Path.Point(70, 55))
+    node_two = Path.Node(1, Path.Point(678, 73))
+    node_three = Path.Node(2, Path.Point(1092, 204))
+    nodearray2 = [node_one, node_two, node_three]
+    
+
+    # Converts the result path to one with nodes from the node array
+    # result_path_1 = g1.convertpath(path_ship_1, nodearray1)
+    # result_path_2 = g1.convertpath(path_ship_2, nodearray1)
+    # result_path_3 = g2.convertpath(path_ship_3, nodearray2)
+
+    # Add these possible paths to the list
+    platform.ship_paths.append(path_ship_1)
+    platform.ship_paths.append(path_ship_2)
+    platform.ship_paths.append(path_ship_3)
+
+    # Create 3 ships
+    platform.ship_list.append(Ship.Ship(100, 100, 5, 15, platform.ship_paths, 1, 2))
+    platform.ship_list.append(Ship.Ship(100, 110, 5, 15, platform.ship_paths, 2, 1))
+    platform.ship_list.append(Ship.Ship(101, 101, 5, 15, platform.ship_paths, 0,0))
+    
+    # for ship in platform.ship_list:
+    #     platform.all_sprites_list.add(ship)
+
+    # --- Determine routes for soldiers once they reach the island
+    platform.island_nodes.append(Path.Node(0, Path.Point(466, 348)))
+    platform.island_nodes.append(Path.Node(1, Path.Point(630, 239)))
+    platform.island_nodes.append(Path.Node(2, Path.Point(805, 259)))
+    platform.island_nodes.append(Path.Node(3, Path.Point(916, 484)))
+    platform.island_nodes.append(Path.Node(4, Path.Point(800, 579)))
+    platform.island_nodes.append(Path.Node(5, Path.Point(555, 560)))
+    # ex_pos1 = [355, 418]
+    ex_pos1 = [875, 682]
+    # onepath = find_route(ex_pos1, 4, platform.island_nodes)
+    onepath = find_route(ex_pos1, 2, platform.island_nodes)
+    print(onepath)
+
+
 
     pos = pygame.mouse.get_pos()
 
@@ -302,6 +483,20 @@ if __name__ == '__main__':
 
             #------------ /CHECKING FOR CONTACT WITH MINES--------------------
 
+            #------------ / checking for contact of ships with Aquatic Mines --------
+
+            for ship in platform.ship_list:
+                for mine in platform.aquamine_list:
+                    d=calcDistance(mine.x, mine.y, ship.x, ship.y)
+                    if d < 50:
+                        #print("ship hit")
+                        ship.enemies_on_board-= 3
+                        platform.aquamine_list.remove(mine)
+                        platform.all_sprites_list.remove(mine)
+
+
+            #------------checking for contact of ships with Aquatic Mines --------
+
             #------------ CANNON FIRING ------------------------------
 
             if len(platform.activeCannonBalls) > 0:
@@ -314,6 +509,16 @@ if __name__ == '__main__':
 
             #------------ /CANNON FIRING ------------------------------
 
+            #------------ SHIP MOVEMENTS ------------------------------
+
+            for ship in platform.ship_list:
+                ship.updateShip(platform)
+                if ship.is_destroyed:
+                    platform.ship_list.remove(ship)
+                else:
+                    ship.render(gameDisplay)
+
+            #------------ /SHIP MOVEMENTS ------------------------------
 
 
 
--- Ship.py
+++ Ship.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 from random import randint
+from Movement import *
+from Army import *
 
 DISPLAY_WIDTH = 124
 DISPLAY_HEIGHT = 92
@@ -10,36 +12,83 @@ RED = (255, 25, 25)
 SHIP_IMG = pygame.image.load("ship.png")
 SHIP_IMG = pygame.transform.scale(SHIP_IMG, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
-class Ship(pygame.sprite.Sprite):
-    hits = 0
-    capacity = 100
+class Ship:
+    
     image = SHIP_IMG
 
-    def __init__(self, x, y, velocity, max_vel, paths):
+    def __init__(self, x, y, velocity, max_vel, paths,path_no,t):
         # Call the parent class (Sprite) constructor
-        super().__init__()
+        
 
         self.x = x
         self.y = y
         self.x_vel = 0
         self.y_vel = 0
-        self.vel = velocity
+        self.vel = randint(velocity, max_vel)
         self.max_vel = max_vel
-        self.path = paths[randint(0,2)]
+        self.path = paths[path_no]
+        self.target_pier = t
+        self.present_seek = self.path[0]
+
+        self.enemies_on_board = 6
+        self.reached_pier = False
+        self.is_destroyed = False
 
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
-    def move(self, angle, wrap=False):
+    
+    def updateShip(self, platform):
+        if not self.reached_pier:
+            #debugging
+            #print("updating")
+            #print("seeking : "+str(self.present_seek))
+            if self.enemies_on_board == 0:
+                self.is_destroyed = True
 
-        if (wrap == False) and ((self.x > DISPLAY_WIDTH - 20) or (self.y > DISPLAY_HEIGHT - 40)):
-            self.vel = 0
+            target_x= platform.water_nodes[self.present_seek][0]
+            target_y= platform.water_nodes[self.present_seek][1]
 
-        self.x += self.vel * math.cos(math.radians(angle))
-        self.y += self.vel * math.sin(math.radians(angle))
+            present_seek_done = object_seek(Soldier(target_x, target_y, 1,'player'), self, self.vel)
 
-        if wrap == True:
-            self.x %= DISPLAY_WIDTH
-            self.y %= DISPLAY_HEIGHT
+            if present_seek_done:
+                self.path.remove(self.present_seek)
+                self.present_seek_done = False
+                if len(self.path) > 0:
+                    self.present_seek = self.path[0]
+                else:
+                    self.reached_pier = True
+                    self.disembark(platform)
+
+    def render(self, gameDisplay):
+        pygame.draw.circle(gameDisplay, WHITE,(self.x, self.y), 7)
+
+    def disembark(self,platform):
+        print("disembarking")
+        disembarking_points = platform.disembarking_points[self.target_pier]
+        platoon_number = len(platform.enemyPlatoons) + 1
+        for i in range(self.enemies_on_board):
+            x_cord=disembarking_points[i][0]
+            y_cord=disembarking_points[i][1]
+            platform.enemyArmy.append(Soldier(x_cord,y_cord, platoon_number, 'enemy'))
+        platform.enemyPlatoons.append(Platoon(platform,platoon_number,'enemy'))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
